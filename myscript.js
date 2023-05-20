39a8aa2e7e539d46a728c56ace68a0891f7fdb46
Wilson Huang <wilsonhuang11012000@gmail.com>
2023-05-19 20:20:34 -0400

    Add the required framework for etch-a-sketch

---
+++ myscript.js
@@ -0,0 +1,46 @@
+let gridNumber = 0;
+const body = document.querySelector("body");
+const button = document.querySelector("button");
+button.textContent = "Change Grid Size";
+button.addEventListener("click", () => {
+    gridNumber = prompt("What by what grid would you like? (Up to 100)");
+    if(gridNumber <= 100){
+    deleteGrid();
+    makeGrid(gridNumber);
+    }
+    else{
+        alert("You chose a number over 100 please try again!");
+    }
+})
+function makeGrid (gridNumber = 16){
+const container = document.querySelector(".container");
+for (let i = 0; i < gridNumber; i++){
+    const rowDiv = document.createElement("div");
+    rowDiv.classList.add("row_div");    
+    container.append(rowDiv);
+    for(let j = 0; j < gridNumber; j++){
+        const colDiv = document.createElement("div");
+        colDiv.classList.add("border");
+        cellSize = 960/gridNumber;
+        colDiv.style.height = cellSize + "px";
+        colDiv.style.width = cellSize + "px";
+        rowDiv.append(colDiv);
+    }
+}
+const allDiv = document.querySelectorAll(".border")
+
+allDiv.forEach((div)=>{
+    div.addEventListener("mouseover", () => {
+        div.classList.add("color_blue");
+    })
+})
+}
+
+function deleteGrid(){
+    const allDiv = document.querySelectorAll(".border")
+    allDiv.forEach((div)=>{
+        div.remove()
+        })
+    }
+makeGrid(16);
+
